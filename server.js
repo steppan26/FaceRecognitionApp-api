6b0fbb8f05aa25d7f8c350a0183308200d508cff
Stephane Baroux <stephane.baroux@gmail.com>
2021-08-06 12:02:27 +0200

    quick fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({ // create variable which uses database called 'bd' using knex.
         connectionString : process.env.DATABASE_URL,
         ssl: true
     }
-  });
+});
 
 const app = express()
 app.use(express.json())
@@ -23,18 +23,12 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => { res.send('it is working!')})
-
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // add the dependencies ('db' and 'bcrypt') for use in ./controllers/register.js [also known as dependency injection]
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port ${process.env.PORT}`)
-    console.log(`app is running on port ${process.env.DATABASE_URL}`)
 })
