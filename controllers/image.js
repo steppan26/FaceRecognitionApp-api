3129f976639f5e4f14aaab5d1dc5ce3f1f488689
Stephane Baroux <stephane.baroux@gmail.com>
2021-08-08 15:19:58 +0200

    console log the data

--- controllers/image.js
+++ controllers/image.js
@@ -10,6 +10,7 @@ const handleApiCall = (req, res) => {
     .then(data => {
         res.json(data)
     })
+    .then(data => console.log(data))
     .catch(err => res.status(400).json('unable to work with API'))
 }
 
